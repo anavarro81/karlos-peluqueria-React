aae76b802731c83d09439f4b6895c65c0eaacca2
Antonio Navarro <dojeDulDev@Proton.me>
2026-01-15 18:43:09 +0100

    feat(gallery): include dog and description name

--- src/data/imageGallery.js
+++ src/data/imageGallery.js
@@ -1,15 +1,15 @@
 import dog_small_1 from '../assets/Galley_Dog_image_small_1.webp';
 import dog_small_2 from '../assets/Galley_Dog_image_small_2.webp';
 import dog_small_3 from '../assets/Galley_Dog_image_small_3.webp';
-// import dog_small_4 from '../assets/Galley_Dog_image_small_4.webp';
+import dog_small_4 from '../assets/Galley_Dog_image_small_4.webp';
 // import dog_small_5 from '../assets/Galley_Dog_image_small_5.webp';
 // import dog_small_6 from '../assets/Galley_Dog_image_small_6.webp';
 
 const imageGallery = [
-  { id: 1, url: dog_small_1, alt: 'perro peluqueria 1', title: 'perro 1', description: 'descripción perro 1' },
+  { id: 1, url: dog_small_1, alt: 'perro peluqueria 1', title: 'Tyrion', description: 'Independiente,fiel,guardián y súper guapo' },
   { id: 2, url: dog_small_2, alt: 'perro peluqueria 2', title: 'perro 2', description: 'descripción perro 2' },
   { id: 3, url: dog_small_3, alt: 'perro peluqueria 3', title: 'perro 3', description: 'descripción perro 3' },
-  // { id: 4, url: dog_small_4, alt: 'perro peluqueria 4', title: 'perro 4', description: 'descripción perro 4' },
+  { id: 4, url: dog_small_4, alt: 'perro peluqueria 4', title: 'Rita', description: 'Cariñosisima , buena y juguetona' },
   // { id: 5, url: dog_small_5, alt: 'perro peluqueria 5', title: 'perro 5', description: 'descripción perro 5' },
   // { id: 6, url: dog_small_6, alt: 'perro peluqueria 6', title: 'perro 6', description: 'descripción perro 6' },
 ];
